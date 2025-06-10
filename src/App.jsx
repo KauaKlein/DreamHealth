d78608a0c9c7fc4b7ac30cf0af3e030b01155d38
JoaoCarlosForment0 <jcformento13@gmail.com>
2025-06-10 17:05:57 -0300

    Inicio do dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (prefersDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
 
   return (
     <>
