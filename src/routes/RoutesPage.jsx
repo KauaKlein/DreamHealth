309220c87a995fece926f0cea8bef7e716785086
KAUÃ ROSA KLEIN <kaua_klein@estudante.sesisenai.org.br>
2025-06-11 14:59:21 -0300

    correção das rotas

--- src/routes/RoutesPage.jsx
+++ src/routes/RoutesPage.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../components/layout/Layout";
-import { HomePage } from "../pages/HomePage";
+import HomePage from "../pages/HomePage"; 
 import { LoginPage } from "../pages/LoginPage";
 import { CadastroPage } from "../pages/CadastroPage";
 import { DashBoardPage } from "../pages/DashBoardPage";
@@ -12,9 +12,11 @@ export const RoutesPage = () =>{
             <Routes>
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<HomePage/>}/>
+                    <Route path="/dashBoard" element={<DashBoardPage/>}/>
+                </Route>
+                <Route>
                     <Route path="/login" element={<LoginPage/>}/>
                     <Route path="/cadastro" element={<CadastroPage/>}/>
-                    <Route path="/dashBoard" element={<DashBoardPage/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
